921b215b9bb34188e35026e91b7a30ae24498b3e
John Ruble <john.ruble@atomicobject.com>
2024-03-11 11:06:39 -0400

    desktop/index.js from remix-electron readme

---
+++ desktop/index.js
@@ -0,0 +1,21 @@
+// desktop/index.js
+const { initRemix } = require("remix-electron")
+const { app, BrowserWindow } = require("electron")
+const { join } = require("node:path")
+
+/** @type {BrowserWindow | undefined} */
+let win
+
+app.on("ready", async () => {
+	try {
+		const url = await initRemix({
+			serverBuild: join(__dirname, "../build/index.js"),
+		})
+
+		win = new BrowserWindow({ show: false })
+		await win.loadURL(url)
+		win.show()
+	} catch (error) {
+		console.error(error)
+	}
+})
